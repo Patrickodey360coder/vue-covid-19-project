e68ef039833392e8feecfed09e0478e5c4b3b32b
patrick <patrickodey419@gmail.com>
2021-07-05 07:46:02 +0100

    formatted API date with moment.js and create a new component for the data boxes

--- src/components/DataTitle.vue
+++ src/components/DataTitle.vue
@@ -2,15 +2,22 @@
   <div>
     <p>hello</p>
     <h1>{{text}}</h1>
-    <h1>{{dataDate}}</h1>
+    <h1>{{timestamp}}</h1>
   </div>
 </template>
 
 <script>
+import moment from "moment"  
 
 export default {
+
   name: 'DataTile',
-  props: ['text', 'dataDate']
+  props: ['text', 'dataDate'],
+  computed: {
+    timestamp: function(){
+      return moment(this.dataDate).format("MMMM Do YYYY, h:m:s")
+    }
+  }
 }
 </script>
 
